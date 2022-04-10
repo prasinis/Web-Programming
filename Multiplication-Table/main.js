c72ee6195a3afb2bc3079a1985447fc681000259
Prodromos Nasis <79412734+pnasis@users.noreply.github.com>
2022-04-11 02:22:20 +0300

    Update main.js

--- Multiplication-Table/main.js
+++ Multiplication-Table/main.js
@@ -2,9 +2,9 @@
 
 let tries = 0;
 let success = 0;
-const numbers = [2,3,4,5,6,7,8,9] //*Οι αριθμοί για τους οποίους θα δοκιμάσω τη προπαίδεια.
+const numbers = [2,3,4,5,6,7,8,9] //*Numbers we are going to use on our multiplication table.
 
-let choice = prompt('Για ποιον αριθμό θέλεις να δοκιμάσεις;')
+let choice = prompt('With what number do you want to try?')
 
 let h3 = document.querySelector('h3');
 let h4 = document.querySelector('h4');
@@ -20,15 +20,15 @@ function ask(){
     <button>OK<button>
     `
     document.querySelector('button').addEventListener('click', function(){
-        if(numbers[i] * choice == document.querySelector('#answer').value){ //#answer=χρησιμοποιώντας το id του answer.
-             h4.innerText = 'Συγχαρητήρια!'
+        if(numbers[i] * choice == document.querySelector('#answer').value){ //#answer = we use the id of the textfield answer.
+             h4.innerText = 'Congratulations!'
              success++;
              i++;
         } else{
-            h4.innerText = 'Δοκίμασε ξανά..'
+            h4.innerText = 'Try again..'
         }
         tries++;
-        document.querySelector('h2').innerText = `Σκόρ: ${success} στις ${tries} προσπάθειες`
+        document.querySelector('h2').innerText = `Score: ${success} from ${tries} tries`
         if(i < numbers.length) ask();
     })
 }
